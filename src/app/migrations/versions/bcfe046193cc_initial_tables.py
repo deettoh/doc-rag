7674d2ecfd869c25760159e6a126e5c816645749
jay <hu.jay.hu.jay@gmail.com>
2026-02-06 01:16:34 +0800

    fix ruff linting

--- src/app/migrations/versions/bcfe046193cc_initial_tables.py
+++ src/app/migrations/versions/bcfe046193cc_initial_tables.py
@@ -6,18 +6,17 @@ Create Date: 2026-02-04 22:09:51.910469
 
 """
 
-from typing import Sequence, Union
+from collections.abc import Sequence
 
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 from pgvector.sqlalchemy import Vector
-
 
 # revision identifiers, used by Alembic.
 revision: str = "bcfe046193cc"
-down_revision: Union[str, Sequence[str], None] = None
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | Sequence[str] | None = None
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
